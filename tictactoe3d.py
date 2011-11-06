8e004440bf833661159f11c0bf33bda7eadb93d6
Martijn Arts <arts.martijn@gmail.com>
2011-11-06 20:24:15 +0100

    Spelling error fixed; expanded smartass computer idea

---
+++ tictactoe3d.py
@@ -0,0 +1,34 @@
+# Checklist of what we need to write:
+#   - gui
+#   - data structure
+#   - Smartass computer
+#     simple idea:
+#     Computer calculates all possible moves, chooses
+#     the one with the highest chance of winning.
+#     Maybe multiple difficulties, with lower difficulties
+#     picking the ones with lower chance of winning (or
+#     even choosing at random).
+
+# Coordinates will be tupples of (x,y,z)
+
+# Constants
+size = 3
+
+# Data functions
+def change_value(coordinates,value,game_space):
+    game_space = None
+    return game_space
+
+def return_value(coordinates):
+    value = None
+    return value
+
+# --- Main ---
+def Main():
+
+    # -- init --
+    Done = False
+
+    # -- Main Loop --
+    while not Done:
+        pass
