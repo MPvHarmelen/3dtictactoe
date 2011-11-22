691eb1cdaca0570eb66ae192bb37b69e425188f7
Martin van Harmelen <Martin@vanharmelen.com>
2011-11-22 21:36:02 +0100

    Moved class Board to seperat file & added a comment

---
+++ Board.py
@@ -0,0 +1,85 @@
+class Board(object):
+    '''This is the object that represents the board.'''
+    def __init__(self, size, amount_players):
+        self.board = {}
+        self.size = size
+        for x in range(size):
+            for y in range(size):
+                for z in range(size):
+                    self.board[(x,y,z)]=None
+
+    def getCoord(self, coords):
+        return self.board[coords]
+
+    def setCoord(self, coords, value):
+        for x in coords:
+            if x > self.size-1:
+                return False
+        # We are defining this out of scope, will it work?
+        self.board[coords] = value
+        return True
+
+    def CheckIfWon(self):
+        # TODO: Think of a good way to check if someone has
+        # won without any kind of limit to the board size.
+        # Just using lines would be quite hard, since there's
+        # multiple diagonals when you have a larger board.
+        #  I think lines would be okay. We just need to edit the
+        #  function below to stand up to (is this correct EN?) our needs
+        #  of a 3D game. We also need to think of names for things like
+        #  collumns and rows that are in different angles. I think that
+        #  last is best to be discussed in real life =).
+        def BoardToLines(self):
+            self.lines = []
+
+            # If we use yield, we can only call it once. I'm not sure if you
+            # know about generators, but if you do:
+            # yield generates a generator =)
+            
+            # (0,0,line) >> (size,size,line)
+            for x in range(size):
+                for y in range(size):
+                    yield [getCoord(self, (x,y,z)) for z in range(size)]
+
+            # (0,line,0) >> (size,line,size)
+            for x in range(size):
+                for z in range(size):
+                    yield [getCoord(self, (x,y,z)) for y in range(size)]
+                    
+
+##            # (line,0,0) >> (line,size,size)
+##            for y in range(size):
+##                for z in range(size):
+##                    self.lines.append(
+            
+            
+            
+
+##def board_to_lines():
+##    lines = []
+##
+##    # Rows
+##    for y_co in range(gridsize):
+##        lines.append([])
+##        for x_co in range(gridsize):
+##            lines[y_co].append(return_cell((x_co,y_co)))
+##    
+##    # Columns
+##    for x_co in range(gridsize):
+##        lines.append([])
+##        for y_co in range(gridsize):
+##            lines[gridsize + x_co].append(return_cell((x_co,y_co)))
+##
+##    # 1st diagonal
+##    lines.append([])
+##    for co in range(gridsize):
+##        lines[gridsize*2].append(return_cell((co,co)))
+##    
+##    # 2nd diagonal
+##    lines.append([])
+##    for co in range(gridsize):
+##        lines[gridsize*2 +1].append(return_cell((co,(gridsize-1)-co)))
+##
+##    return lines
+    
+        pass
--- tictactoe3d.py
+++ tictactoe3d.py
@@ -47,92 +47,10 @@ class Computer(object):
         #  Use function I described in the comment under CheckIfWon
         pass
 
-class Board(object):
-    def __init__(self, size, amount_players):
-        self.board = {}
-        self.size = size
-        for x in range(size):
-            for y in range(size):
-                for z in range(size):
-                    self.board[(x,y,z)]=None
+from Board import Board
 
-    def getCoord(self, coords):
-        return self.board[coords]
-
-    def setCoord(self, coords, value):
-        for x in coords:
-            if x > self.size-1:
-                return False
-        # We are defining this out of scope, will it work?
-        self.board[coords] = value
-        return True
-
-    def CheckIfWon(self):
-        # TODO: Think of a good way to check if someone has
-        # won without any kind of limit to the board size.
-        # Just using lines would be quite hard, since there's
-        # multiple diagonals when you have a larger board.
-        #  I think lines would be okay. We just need to edit the
-        #  function below to stand up to (is this correct EN?) our needs
-        #  of a 3D game. We also need to think of names for things like
-        #  collumns and rows that are in different angles. I think that
-        #  last is best to be discussed in real life =).
-        def BoardToLines(self):
-            self.lines = []
-
-            # (0,0,line) >> (size,size,line)
-            for x in range(size):
-                for y in range(size):
-                    self.lines.append([])
-                    for z in range(size):
-                        self.lines[-1].append(getCoord(self, (x,y,z)))
-
-            # (0,line,0) >> (size,line,size)
-            for x in range(size):
-                for z in range(size):
-                    self.lines.append([])
-                    for y in range(size):
-                        self.lines[-1].append(getCoord(self, (x,y,z)))
-
-##            # (line,0,0) >> (line,size,size)
-##            for y in range(size):
-##                for z in range(size):
-##                    self.lines.append(
-            
-            
-            
-
-##def board_to_lines():
-##    lines = []
-##
-##    # Rows
-##    for y_co in range(gridsize):
-##        lines.append([])
-##        for x_co in range(gridsize):
-##            lines[y_co].append(return_cell((x_co,y_co)))
-##    
-##    # Columns
-##    for x_co in range(gridsize):
-##        lines.append([])
-##        for y_co in range(gridsize):
-##            lines[gridsize + x_co].append(return_cell((x_co,y_co)))
-##
-##    # 1st diagonal
-##    lines.append([])
-##    for co in range(gridsize):
-##        lines[gridsize*2].append(return_cell((co,co)))
-##    
-##    # 2nd diagonal
-##    lines.append([])
-##    for co in range(gridsize):
-##        lines[gridsize*2 +1].append(return_cell((co,(gridsize-1)-co)))
-##
-##    return lines
-    
-        pass
 
 if __name__ == '__main__':
-    Done = False
-    while not Done:
-        pass
-
+##    Done = False
+##    while not Done:
+##        pass
